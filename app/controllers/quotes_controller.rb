ef0d51dd61fdb0f092668f3f7120aa216201e7a3
jonathansalfen <jonathan.salfen@gmail.com>
2020-02-02 13:28:18 -0800

    add quotes controller logic

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,41 @@
+class QuotesController < ApplicationController
+
+  def index
+    @show = Show.find(params[:show_id])
+    @quotes = Quote.all
+    json_response(@quotes)
+  end
+
+  def show
+    @show = Show.find(params[:show_id])
+    @quote = Quote.find(params[:id])
+    json_response(@quote)
+  end
+
+  def create
+    @show = Show.find(params[:show_id])
+    @quote = Quote.create(quote_params)
+    json_response(@quote)
+  end
+
+  def update
+    @show = Show.find(params[:show_id])
+    @quote = Quote.find(params[:id])
+    @quote.update(quote_params)
+  end
+
+  def destroy
+    @show = Show.find(params[:show_id])
+    @quote = Quote.find(params[:id])
+    @quote.destroy
+  end
+
+  private
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def quote_params
+    params.permit(:quotation, :show_id)
+  end
+end
